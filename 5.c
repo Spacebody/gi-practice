fccb9d20d70b2cb701f95efff35236fc99235b3f
spacebody <510662916@qq.com>
2018-05-28 10:45:58 +0800

    add 5.c

---
+++ 5.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 5.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 10:44:47 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argv, char* argv[])
+{
+    printf("This is the fifth version.\n");
+    return 0;
+}
