4cdec26eb22d19e13502d7800fab223813dd5f40
spacebody <510662916@qq.com>
2018-05-28 10:35:39 +0800

    add 3.c

---
+++ 3.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 3.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 10:34:16 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("This is the third version from branch.\n"):
+    return 0;
+}
