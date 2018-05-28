edd6a6e12de5d377632bf5eb8d8815f8fd0e0450
spacebody <510662916@qq.com>
2018-05-28 11:20:09 +0800

    add 8.c

---
+++ 8.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 8.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 11:19:00 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("This is the eighth version and also the last version.\n");
+    return 0;
+}
