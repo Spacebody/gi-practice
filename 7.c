c6c8bd009cac6cd1cd132602e36142a6ddf8cec5
spacebody <510662916@qq.com>
2018-05-28 11:04:43 +0800

    add 7.c

---
+++ 7.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 7.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 11:03:46 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("This is the seventh version.\n");
+    return 0;
+}
