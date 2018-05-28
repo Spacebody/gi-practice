93563584de5c8f4b3eedbbda89fd63b1f22deae6
spacebody <510662916@qq.com>
2018-05-28 10:32:31 +0800

    add 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 2.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 10:30:06 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("This is the second version.\n");
+    return 0;
+}
