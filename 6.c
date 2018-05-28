d379a3cde22fc684c226cecc9d7da4dff7c81739
spacebody <510662916@qq.com>
2018-05-28 10:54:19 +0800

    add 6.c

---
+++ 6.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 6.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一  5/28 10:49:02 2018
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("This is the six version.\n");
+    return 0;
+}
